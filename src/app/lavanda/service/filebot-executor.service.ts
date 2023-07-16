423a0dec75bf919b5c3b3e15b39c8c52b293029d
Luis Carlos <luisca_jl@hotmail.com>
2023-07-16 08:49:38 +0200

    New upgrade on the frontend

--- src/app/lavanda/service/filebot-executor.service.ts
+++ src/app/lavanda/service/filebot-executor.service.ts
@@ -8,57 +8,60 @@ import { Qbittorrent } from '../api/qbittorrent.model';
 
 @Injectable({ providedIn: 'root' })
 export class FilebotExecutorService {
-    constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
 
-    getAllByPageable(page: number, pageSize: number, path?: string, status?: string): Observable<Pageable> {
-        if (environment.testing) {
-            return this.httpClient.get<Pageable>('assets/lavanda/data/dummy-filebot-executor.json');
+  getAllByPageable(page: number, pageSize: number, path?: string, status?: string): Observable<Pageable> {
+    if (environment.testing) {
+      return this.httpClient.get<Pageable>('assets/lavanda/data/dummy-filebot-executor.json');
 
-        }
-        else {
-            let params = new HttpParams();
-            // ?page = ' + page + ' & size=' + pageSize + ' & path=' + path + ' & status=' + status
-            params = params.set('pageSize', pageSize);
-            params = params.set('page', page);
-
-            if (path) {
-                params = params.set('path', path);
-            }
-            if (status) {
-                params = params.set('status', status);
-            }
-            return this.httpClient.get<Pageable>(environment.apiUrl + 'filebot-executor', { params });
-        }
     }
+    else {
+      let params = new HttpParams();
+      // ?page = ' + page + ' & size=' + pageSize + ' & path=' + path + ' & status=' + status
+      params = params.set('pageSize', pageSize);
+      params = params.set('page', page);
+
+      if (path) {
+        params = params.set('path', path);
+      }
+      if (status) {
+        params = params.set('status', status);
+      }
+      return this.httpClient.get<Pageable>(environment.apiUrl + 'filebot-executor', { params });
+    }
+  }
 
 
-    editFilebotExecutor(id: string, filebotExecutor: FilebotExecutor): Observable<FilebotExecutor> {
-        return this.httpClient.patch<FilebotExecutor>(environment.apiUrl + 'filebot-executor/' + id, filebotExecutor);
-    }
+  editFilebotExecutor(id: string, filebotExecutor: FilebotExecutor): Observable<FilebotExecutor> {
+    return this.httpClient.patch<FilebotExecutor>(environment.apiUrl + 'filebot-executor/' + id, filebotExecutor);
+  }
 
-    delete(id: string): Observable<any> {
-        return this.httpClient.delete(environment.apiUrl + 'filebot-executor/' + id);
-    }
+  delete(id: string): Observable<any> {
+    return this.httpClient.delete(environment.apiUrl + 'filebot-executor/' + id);
+  }
 
-    getAllFilebotExecutor(): Observable<string[]> {
-        return this.httpClient.get<string[]>(environment.apiUrl + 'filebot-executor/files');
+  getAllFilebotExecutor(): Observable<string[]> {
+    if (environment.testing) {
+      return this.httpClient.get<string[]>('assets/lavanda/data/dummy-filebot-executor-files.json');
     }
+    else {
+      return this.httpClient.get<string[]>(environment.apiUrl + 'filebot-executor/files');
+    }
+  }
 
-    createQbittorrent(qbittorrent: Qbittorrent): Observable<Qbittorrent> {
-        return this.httpClient.post<Qbittorrent>(environment.apiUrl + 'filebot-executor', qbittorrent);
-    }
+  createQbittorrent(qbittorrent: Qbittorrent): Observable<Qbittorrent> {
+    return this.httpClient.post<Qbittorrent>(environment.apiUrl + 'filebot-executor', qbittorrent);
+  }
 
-    manualExecution(): Observable<Qbittorrent> {
-        return this.httpClient.post<Qbittorrent>(environment.apiUrl + 'filebot-executor/execute', null);
-    }
+  manualExecution(): Observable<Qbittorrent> {
+    return this.httpClient.post<Qbittorrent>(environment.apiUrl + 'filebot-executor/execute', null);
+  }
 
-    manualExecutionShow(): Observable<Qbittorrent> {
-        return this.httpClient.post<Qbittorrent>(environment.apiUrl + 'filebot-executor/execute/show', null);
-    }
-    downloadLog(data: string) {
-        const blob = new Blob([data], { type: 'text/plain' });
-        const url = window.URL.createObjectURL(blob);
-        window.open(url);
-    }
+
+  downloadLog(data: string) {
+    const blob = new Blob([data], { type: 'text/plain' });
+    const url = window.URL.createObjectURL(blob);
+    window.open(url);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    testing: false,
+    testing: true,
     apiUrl: 'http://localhost:8080/'
 };
 
